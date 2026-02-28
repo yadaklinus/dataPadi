8303bde8488f75fc45669fa185bfeaf0e7ae985c
CodeGit <linusyadak@gmail.com>
2026-02-28 14:49:13 +0100

    ok

--- app/user/dashboard/page.tsx
+++ app/user/dashboard/page.tsx
@@ -5,6 +5,8 @@ import { redirect } from 'next/navigation';
 export default async function DashboardPage() {
   const result = await getDashboardData();
 
+  console.log(result)
+
   if (!result.success || !result.data) {
     // If unauthorized or error, kick back to login
     redirect('/auth/login');
